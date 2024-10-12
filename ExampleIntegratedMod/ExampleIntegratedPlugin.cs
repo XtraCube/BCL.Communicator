1c4777883942a00953f232ddcc03a51135cd8925
XtraCube <72575280+XtraCube@users.noreply.github.com>
2024-10-12 18:14:45 -0400

    .

---
+++ ExampleIntegratedMod/ExampleIntegratedPlugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace ExampleIntegratedMod;
+
+[BepInAutoPlugin("com.example.integrated", "Example Integrated Plugin", "1.0.0")]
+[BepInProcess("Among Us.exe")]
+public partial class ExampleIntegratedPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+    public override void Load()
+    {
+        Harmony.PatchAll();
+    }
+
+    public override bool Unload()
+    {
+        Harmony.UnpatchSelf();
+        return base.Unload();
+    }
+}
